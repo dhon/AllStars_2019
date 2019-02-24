5ef67abf16e1ba5e95e4ff1cf489227144bd2d2b
unknown <morgangrimes14@gmail.com>
2019-02-24 02:53:53 -0600

    add games 13-16

--- js/services.js
+++ js/services.js
@@ -13,7 +13,7 @@ angular.module('app.services', [])
     };
 
     function getJSON(){
-        for(var i = 0; i < 12; i++){
+        for(var i = 0; i < 16; i++){
             var ourRequest = new XMLHttpRequest();
             var num = (i+1);
             if (num < 10) num = '0' + num;
