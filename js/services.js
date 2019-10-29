879134db3afc02a2919f74495291860aa105ff2c
dhon <morgangrimes14@gmail.com>
2019-10-29 11:29:59 -0500

    add new games

--- js/services.js
+++ js/services.js
@@ -13,7 +13,7 @@ angular.module('app.services', [])
     };
 
     function getJSON(){
-        for(var i = 0; i < 71; i++){
+        for(var i = 0; i < 76; i++){
             var ourRequest = new XMLHttpRequest();
             var num = (i+1);
             if (num < 10) num = '0' + num;
